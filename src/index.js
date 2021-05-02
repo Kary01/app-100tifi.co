d1c0dff85a4b1e4f424bce94b0124bc18127e679
Karen Ruiz <anakaren.ruizb@gmail.com>
2021-05-02 18:16:30 -0500

    add hash

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import router from './routes';
 
 window.addEventListener('load', router);
+
+window.addEventListener('hashchange', router);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
         <div class="Characters">
             <article class="Character-item">
                 <a href="#/1/">
-                    <img src="image" alt="name"/>
+                    <img src="image" alt="name">
                     <h2>Name</h2>
                 </a>
             </article>
