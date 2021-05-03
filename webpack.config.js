115232c0f5c8d66472b53029385ac6f206faa838
Karen Ruiz <anakaren.ruizb@gmail.com>
2021-05-03 00:39:02 -0500

    add styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -29,6 +30,12 @@ module.exports = {
                 template: './public/index.html',
                 filename: './index.html',
             }
-        )
+        ),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: './src/styles/style.css',
+                to: '',
+            }]
+        }),
     ]
 }
